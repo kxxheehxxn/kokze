48b6d1bf56cc2c2db4b312ff3d428a021260cca1
kxxheehxxn <fpqk7099@gmail.com>
2025-07-29 11:34:42 +0900

    fix: 브랜치 병합사항 수정

--- front/src/router/auth.js
+++ front/src/router/auth.js
@@ -1,28 +1,25 @@
 export default [
-    {
-        path: '/auth/login',
-        name: 'login',
-        component: () => import('../pages/auth/LoginPage.vue'),
-    },
-    {
-<<<<<<< HEAD
-        path: '/auth/kakao/callback',
-        name: 'kakaoCallback',
-        component: () => import('../pages/auth/KakaoCallbackPage.vue'),
-=======
-        path: '/signup/step1/local',
-        name: 'signup-step1-local',
-        component: () => import('../pages/auth/SignupStep1Local.vue'),
-    },
-    {
-        path: '/signup/step1/kakao',
-        name: 'signup-step1-kakao',
-        component: () => import('../pages/auth/SignupStep1Kakao.vue'),
-    },
-    {
-        path: '/signup/step2',
-        name: 'signup-step2',
-        component: () => import('../pages/auth/SignupStep2.vue'),
->>>>>>> develop
-    },
-];
+  {
+    path: '/auth/login',
+    name: 'login',
+    component: () => import('../pages/auth/LoginPage.vue'),
+  },
+  {
+    path: '/auth/kakao/callback',
+    name: 'kakaoCallback',
+    component: () => import('../pages/auth/KakaoCallbackPage.vue'),
+    path: '/signup/step1/local',
+    name: 'signup-step1-local',
+    component: () => import('../pages/auth/SignupStep1Local.vue'),
+  },
+  {
+    path: '/signup/step1/kakao',
+    name: 'signup-step1-kakao',
+    component: () => import('../pages/auth/SignupStep1Kakao.vue'),
+  },
+  {
+    path: '/signup/step2',
+    name: 'signup-step2',
+    component: () => import('../pages/auth/SignupStep2.vue'),
+  },
+]

3692bb2840011da96c9f8cac03425a688e555d50
KJK <coma0786@naver.com>
2025-08-04 14:01:10 +0900

    refactor: 금융 상품 리스트 조회 페이지네이션 수정

--- back/src/main/java/org/ozea/product/controller/ProductController.java
+++ back/src/main/java/org/ozea/product/controller/ProductController.java
@@ -2,11 +2,9 @@ package org.ozea.product.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.ozea.product.dto.request.ProductFilterRequestDto;
-import org.ozea.product.dto.request.ProductListRequestDto;
 import org.ozea.product.dto.response.MbtiRecommendResponseDto;
 import org.ozea.product.dto.response.ProductDetailResponseDto;
 import org.ozea.product.dto.response.ProductListResponseDto;
-import org.ozea.product.dto.response.ProductResponseDto;
 import org.ozea.product.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,17 +22,21 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<?> getProductList(@ModelAttribute ProductListRequestDto requestDto) {
-        List<ProductListResponseDto> products = productService.getProductList(requestDto.getPage(), requestDto.getSize());
+    public ResponseEntity<?> getProductList(@RequestParam(defaultValue = "1") int page,
+                                            @RequestParam(defaultValue = "8") int size) {
+
+        List<ProductListResponseDto> products = productService.getProductList(page, size);
         int totalCount = productService.getTotalProductCount();
+        int totalPages = (int) Math.ceil((double) totalCount / size);
 
         Map<String, Object> result = new HashMap<>();
         result.put("products", products);
-        result.put("totalPages", (int) Math.ceil((double) totalCount / requestDto.getSize()));
-        result.put("currentPage", requestDto.getPage());
+        result.put("totalPages", totalPages);
+        result.put("currentPage", page);
 
         return ResponseEntity.ok(result);
     }
+
 
     @GetMapping("/{finPrdtCd}")
     public ResponseEntity<ProductDetailResponseDto> getProductDetail(@PathVariable String finPrdtCd) {

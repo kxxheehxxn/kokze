254cd19e92e6c498a3f70579299e9557b862b141
yeomsky95 <yeomsky95@gmail.com>
2025-08-13 10:03:31 +0900

    일단 저장

--- back/src/main/java/org/ozea/api/allaccount/mapper/AllAccountMapper.java
+++ back/src/main/java/org/ozea/api/allaccount/mapper/AllAccountMapper.java
@@ -1,10 +1,20 @@
 package org.ozea.api.allaccount.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.ozea.api.allaccount.dto.request.AllAccountReqDto;
 
 @Mapper
 public interface AllAccountMapper {
     AllAccountReqDto getAccountInfo();
     AllAccountReqDto getYeomsky95AccountInfo();
+
+    // UPSERT
+    void upsertBankAccount(
+            @Param("userId") String userId,        // 문자열 UUID
+            @Param("bankName") String bankName,
+            @Param("accountNum") String accountNum,
+            @Param("accountType") String accountType,
+            @Param("balance") Long balance
+    );
 }

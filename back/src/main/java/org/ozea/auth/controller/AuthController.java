3493c8a8fb7f9828ddb41bcb8c41fc96aa4e89c8
kxxheehxxn <fpqk7099@gmail.com>
2025-11-12 21:04:23 +0900

    feat: Setting Spring Security, Add xml file for Scanning

--- back/src/main/java/org/ozea/auth/controller/AuthController.java
+++ back/src/main/java/org/ozea/auth/controller/AuthController.java
@@ -7,6 +7,8 @@ import org.ozea.auth.service.LocalAuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -29,5 +31,18 @@ public class AuthController {
     @PostMapping("/kakao")
     public ResponseEntity<Map<String, Object>> kakao(@RequestBody Map<String, String> body) {
         return ResponseEntity.ok(kakaoAuthService.loginWithKakao(body.get("code")));
+    }
+
+    @GetMapping("/me")
+    public Map<String, Object> me(Principal principal) {
+        Map<String, Object> res = new HashMap<>();
+        if (principal == null) {
+            res.put("authenticated", false);
+            res.put("username", null);
+        }else {
+            res.put("authenticated", true);
+            res.put("username", principal.getName());
+        }
+        return res;
     }
 }
